97887c0cc26ac7a705dbdbda91947672f35cff76
Carole <c.zenruffinen@hotmail.com>
2024-04-11 06:49:08 +0000

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * insertion_sort_list - function to sort an array of ints in ascending order
+ * @list: pointer to pointer to struct list
+ * Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *current_node;
+	listint_t *comparing_node;
+	listint_t *temp;
+
+	current_node = (*list)->next; /* set current node to index 0 */
+	while (current_node != NULL)
+	{
+	comparing_node = current_node->next; /* set comparing_node to index 2 */
+	}
+
+	while (comparing_node != NULL && comparing_node->n < current_node->n)
+	{
+		temp = comparing_node;
+		comparing_node = current_node;
+		current_node = temp;
+
+		comparing_node->next = current_node;
+		current_node->prev = comparing_node;
+
+	}
+
+}
