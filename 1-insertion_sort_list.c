40fd14e7de87086b1ba1d5f06a5000134af0e9c1
Carole <c.zenruffinen@hotmail.com>
2024-04-11 06:54:59 +0000

    update function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * insertion_sort_list - function to sort an array of ints in ascending order
@@ -14,21 +15,21 @@ void insertion_sort_list(listint_t **list)
 	listint_t *comparing_node;
 	listint_t *temp;
 
-	current_node = (*list)->next; /* set current node to index 0 */
+	current_node = (*list); /* set current node to index 0 */
 	while (current_node != NULL)
 	{
-	comparing_node = current_node->next; /* set comparing_node to index 2 */
+		comparing_node = current_node->next; /* set comparing_node to index 2 */
+	
+		while (comparing_node != NULL && comparing_node->n < current_node->n)
+		{
+			temp = comparing_node;
+			comparing_node = current_node;
+			current_node = temp;
+
+			comparing_node->next = current_node;
+			current_node->prev = comparing_node;
+		}
 	}
-
-	while (comparing_node != NULL && comparing_node->n < current_node->n)
-	{
-		temp = comparing_node;
-		comparing_node = current_node;
-		current_node = temp;
-
-		comparing_node->next = current_node;
-		current_node->prev = comparing_node;
-
-	}
+	print_list(*list);
 
 }
