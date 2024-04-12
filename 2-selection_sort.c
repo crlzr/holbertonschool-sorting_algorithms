f8353b86fbe247d15d363dc9c6775736acc2c475
Carole <c.zenruffinen@hotmail.com>
2024-04-12 04:20:28 +0000

    update function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,23 +15,28 @@ void selection_sort(int *array, size_t size)
 	size_t j = 0;
 	size_t min_idx;
 	int temp;
+	int flag = 0;
+
 	/* loop through array */
 	for (i = 0; i < size; i++)
 	{
 		/* find the minimum element in unsorted array */
 		min_idx = i;
-
+		flag = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if ((array[j]) < (array[min_idx]))
 			{
 				min_idx = j;
+				flag = flag + 1;
 			}
 		}
 			/* swap */
 			temp = array[i];
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
-			print_array(array, size);
+
+			if (flag != 0)
+				print_array(array, size);
 	}	
 }
